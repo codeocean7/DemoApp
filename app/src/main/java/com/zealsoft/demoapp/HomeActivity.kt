01df5eab4125b14dd7fbccdcc1ebbcfd4acce78c
ashishBendale777 <ashishbendale777@gmail.com>
2021-02-24 10:28:16 +0530

    Layout added

--- app/src/main/java/com/zealsoft/demoapp/HomeActivity.kt
+++ app/src/main/java/com/zealsoft/demoapp/HomeActivity.kt
@@ -1,11 +1,14 @@
 package com.zealsoft.demoapp
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_home)
+        val img=findViewById<ImageView>(R.id.img)
     }
 }
