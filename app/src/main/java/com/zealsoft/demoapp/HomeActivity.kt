68bcadda5b1ab3f478da12e41d8a12548400f501
ashishbendale777 <ashishbendale777@gmail.com>
2021-02-24 10:23:19 +0530

    New Activity Added

---
+++ app/src/main/java/com/zealsoft/demoapp/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.zealsoft.demoapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
